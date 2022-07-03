bb2e21fff9017b7659681908a9c7f1caaa7e8cc2
AwfAwe <929016211@qq.com>
2022-07-03 13:40:19 +0800

    add Timer and fix some bugs

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,18 @@
 #define MINE_NUM 10
 #define IMAGE_MAX 21
 #include<iostream>
+#include<chrono>
 #include<stdio.h>
 #include<graphics.h>
 #include<math.h>
 #include<string>
 #include <mmsystem.h>
 #include <conio.h>
+
+using namespace std;
+using namespace std::chrono;
+
+
 IMAGE img[21];
 int music_flag = 0;
 int short_speed = 500000;
@@ -49,6 +55,7 @@ enum ATTR
 	M_LONG,
 	M_SHORT
 };
+
 enum MINE {//图片数组下标
 	littlegold = 1,
 	midgold = 3,
@@ -62,6 +69,7 @@ enum MINE {//图片数组下标
 	over = 18,
 	bk = IMAGE_MAX - 1,
 };
+
 struct Role
 {
 	int x;
@@ -70,6 +78,7 @@ struct Role
 	int height;
 	int coin;
 };
+
 struct Role role;
 
 struct Hook
@@ -86,6 +95,7 @@ struct Hook
 	int state; //当前状态
 	int index;//抓住的物品下标
 };
+
 struct Hook hook;
 
 struct Mine  //物品
@@ -98,8 +108,26 @@ struct Mine  //物品
 	bool flag;
 	int gold;
 };
+
 Mine mine[MINE_NUM];
-using namespace std;
+
+double Dis(int A, int B) {
+	double x2 = (mine[A].x - mine[B].x) * (mine[A].x - mine[B].x);
+	double y2 = (mine[A].y - mine[B].y) * (mine[A].y - mine[B].y);
+	return sqrt(x2 + y2);
+}
+
+bool CheckForCover(int x) {
+	for (int i = 0; i < x; ++i) {
+		double dix = Dis(x, i);
+		if (fabs(mine[x].x - mine[i].x) < mine[x].sizex + mine[i].sizex
+			&& fabs(mine[x].y - mine[i].y) < mine[x].sizey + mine[i].sizey) {
+			return false;
+		}
+	}
+	return true;
+}
+
 
 void gameinit()
 {
@@ -118,7 +146,7 @@ void gameinit()
 	hook.state = M_NORMAL;
 	hook.index = -1;
 
-	for (int i = 0; i < MINE_NUM; i++)
+	for (int i = 0; i < MINE_NUM; i+=CheckForCover(i))
 	{
 		mine[i].x = rand() % (WIDTH - 220) + 110;
 		mine[i].y = rand() % (HEIGHT - 220) + 110;
@@ -128,41 +156,44 @@ void gameinit()
 		case LITTLEGOLD:
 			mine[i].sizex = 32;
 			mine[i].sizey = 30;
-			mine[i].gold = rand() % 50 + 25;
+			mine[i].gold = 50;
 			break;
 		case MIDGOLD:
 			mine[i].sizex = 59;
 			mine[i].sizey = 55;
-			mine[i].gold = rand() % 50 + 100;
+			mine[i].gold = 100;
 			break;
 		case BIGGOLD:
 			mine[i].sizex = 155;
 			mine[i].sizey = 144;
-			mine[i].gold = rand() % 50 + 200;
+			mine[i].gold = 500;
 			break;
 		case LITTLESTONE:
 			mine[i].sizex = 67;
 			mine[i].sizey = 57;
-			mine[i].gold = rand() % 50;
+			mine[i].gold = 11;
 			break;
 		case BIGSTONE:
 			mine[i].sizex = 94;
 			mine[i].sizey = 83;
-			mine[i].gold = rand() % 50 + 35;
+			mine[i].gold = 20;
 			break;
 		case MONEY:
 			mine[i].sizex = 73;
 			mine[i].sizey = 83;
-			mine[i].gold = rand() % 250;
+			mine[i].gold = rand() % 500 + 100;
+			//if (!CheckForCover(i)) i--;
+			
 			break;
 		case DIAMOND:
 			mine[i].sizex = 33;
 			mine[i].sizey = 27;
-			mine[i].gold = rand() % 600;
+			mine[i].gold = 600;
 			break;
 		}
 	}
 }
+
 void loadImg()
 {
 	for (int i = 0; i < 20; i++)
@@ -173,8 +204,33 @@ void loadImg()
 	}
 	loadimage(img + 20, "G:\\images\\bk.jpg", WIDTH, HEIGHT);
 }
+
+
+steady_clock::time_point StartTime, IntTime;
+
+typedef int Mode_t;
+duration<double> time_span;
+
+
+int TimeCal(Mode_t Mode) {
+	IntTime = steady_clock::now();
+	switch (Mode){
+		case 1:
+			time_span = duration_cast<duration<double>>(IntTime - StartTime);
+			return time_span.count();
+			break;
+		
+		case 2:
+			break;
+		default:
+			break;
+	}
+
+}
+
 void gamedraw()
 {
+
 	//绘制背景
 	setfillcolor(YELLOW);
 	solidrectangle(0, 0, WIDTH, role.height);
@@ -185,11 +241,14 @@ void gamedraw()
 
 	//输出钱
 	setbkmode(TRANSPARENT);//设置背景透明
-	settextstyle(50, 0, "黑体");
+	settextstyle(40, 0, "黑体");
 	settextcolor(BLACK);
 	char coin[20] = "";
+	char timer[20] = "";
 	sprintf(coin, "金钱：%d", role.coin);
-	outtextxy(20, 20, coin);
+	sprintf(timer, "时间：%d", TimeCal(1));
+	outtextxy(20, 10, coin);
+	outtextxy(20, 60, timer);
 
 	//绘制钩子
 	setlinecolor(RED);
@@ -251,17 +310,23 @@ void hookRock()
 		hook.endy = hook.y + cos(PI / 180 * hook.angle) * hook.len;
 	}
 }
+
 double distance(Hook h)
 {
 	int dis = sqrt((double)(hook.endx - hook.x) * (hook.endx - hook.x) + (hook.endy - hook.y) * (hook.endy - hook.y));
 	return dis <= h.len;
 }
+
 void HookMove() {//钩子位置变化
+	static int cnt = 0;
 	if (hook.state == M_LONG) {//出勾时，钩子位置一直变化
 		hook.endx += hook.vx;
 		hook.endy += hook.vy;
-		for (int i = 0; i < 500000; i++)
-			while (0);
+		DWORD o;
+		cnt++;
+		if (cnt & 1)
+			Sleep(1);
+		else cnt = 0;
 	}
 	else if (hook.state == M_SHORT) {//收勾时，钩子位置一直变化
 		hook.endx -= hook.vx;
@@ -273,6 +338,7 @@ void HookMove() {//钩子位置变化
 		}
 	}
 }
+
 void keycontrol(double speed)
 {
 	if (GetAsyncKeyState(VK_DOWN) && hook.state == M_NORMAL)
@@ -285,6 +351,7 @@ void keycontrol(double speed)
 	if (hook.endx<0 || hook.endx>WIDTH || hook.endy > HEIGHT)
 		hook.state = M_SHORT;
 }
+
 void JudgeGrap() {// 判断是否抓住物品
 	for (int i = 0; i < MINE_NUM; i++) {
 		if (mine[i].flag == true &&//物品存在，且钩子在金块内部，则视为抓住金块，记录金块下标。
@@ -310,24 +377,24 @@ void JudgeGrap() {// 判断是否抓住物品
 		{
 			switch (mine[hook.index].type) {
 			case LITTLEGOLD:
-				short_speed = 1000000;
+				short_speed = 100000;
 				break;
 			case MIDGOLD:
-				short_speed = 10000000;
+				short_speed = 1000000;
 				break;
 			case BIGGOLD:
-				short_speed = 30000000;
+				short_speed = 3000000;
 			case LITTLESTONE:
-				short_speed = 10000000;
+				short_speed = 1000000;
 				break;
 			case BIGSTONE:
-				short_speed = 30000000;
+				short_speed = 3000000;
 				break;
 			case MONEY:
-				short_speed = 1000000;
+				short_speed = 100000;
 				break;
 			case DIAMOND:
-				short_speed = 1000000;
+				short_speed = 100000;
 				break;
 			}
 		}
@@ -337,12 +404,13 @@ void JudgeGrap() {// 判断是否抓住物品
 
 int main()
 {
+
 	initgraph(WIDTH, HEIGHT);   // 创建绘图窗口，大小为 640x480 像素
 	loadImg();
-
 	gameinit();
 
 
+	StartTime = steady_clock::now();
 	while (1)
 	{
 		hookRock();
