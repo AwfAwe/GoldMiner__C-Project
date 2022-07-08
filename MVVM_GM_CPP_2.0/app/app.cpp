853a5fe33a0fed8779e315f89536fd7ab2c1628c
menci725 <97275048+menci725@users.noreply.github.com>
2022-07-09 00:37:31 +0800

    Add files via upload

--- MVVM_GM_CPP_2.0/app/app.cpp
+++ MVVM_GM_CPP_2.0/app/app.cpp
@@ -8,7 +8,6 @@ void app::run(){
 
     model_ptr = std::make_shared<model>();
     viewmodel_ptr = std::make_shared<ViewModel>();
-
     viewmodel_ptr->SetModel(model_ptr);
 
     _mainwindow.SetPlayersPosX(viewmodel_ptr->GetPlayersPosX());
@@ -28,11 +27,14 @@ void app::run(){
     _mainwindow.SetMinesType(viewmodel_ptr->GetMinesType());
     _mainwindow.SetMinesExist(viewmodel_ptr->GetMinesExist());
 
+    _mainwindow.SetLevelGoal(viewmodel_ptr->GetLevelGoal());
+    _mainwindow.SetBoomsNum(viewmodel_ptr->GetBoomsNum());
+
     _mainwindow.setFrameRunCommand(viewmodel_ptr->GetFrameRun());
     _mainwindow.setPlayerSetCommand(viewmodel_ptr->GetPlayerSet());
-
-    _mainwindow.SetTimer();
-//    std::cout << "WHT2"<<std::endl;
+    _mainwindow.setGameResetCommand(viewmodel_ptr->GetGameReset());
+    _mainwindow.setItemsBuyCommand(viewmodel_ptr->GetItemsBuy());
+    _mainwindow.setUseBoomCommand(viewmodel_ptr->GetUseBoom());
     _mainwindow.show();
 }
 
@@ -41,5 +43,5 @@ view* app::GetMainWindow(){
 }
 
 std::shared_ptr<ViewModel> app::GetViewModel(){
-
+    return viewmodel_ptr;
 }
